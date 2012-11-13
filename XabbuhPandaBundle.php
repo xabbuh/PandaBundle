980720e6b59cc45a0180557a7a0b698449ddc7ee
Christian Flothmann <christian.flothmann@xabbuh.de>
2012-11-13 15:17:02 +0100

    initialise XabbuhPandaBundle

---
+++ XabbuhPandaBundle.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+* This file is part of the XabbuhPandaBundle package.
+*
+* (c) Christian Flothmann <christian.flothmann@xabbuh.de>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Xabbuh\PandaBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * XabbuhPandaBundle provides an interface for using the
+ * {@link http://www.pandastream.com/ Panda Video Encoding Service} to encode
+ * video files.
+ * 
+ * @author Christian Flothmann <christian.flothmann@xabbuh.de> 
+ */
+class XabbuhPandaBundle extends Bundle
+{
+    
+}
