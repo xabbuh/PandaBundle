6deacfff1e7edc130ebe39b1690f0019005b00c7
Christian Flothmann <christian.flothmann@xabbuh.de>
2013-01-13 17:56:03 +0100

    upload videos via command-line

---
+++ Command/UploadVideoCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+* This file is part of the XabbuhPandaBundle package.
+*
+* (c) Christian Flothmann <christian.flothmann@xabbuh.de>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Xabbuh\PandaBundle\Command;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Command for upload video files into a Panda cloud.
+ *
+ * @author Christian Flothmann <christian.flothmann@xabbuh.de>
+ */
+class UploadVideoCommand extends CloudCommand
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function configure()
+    {
+        $this->setName("panda:video:upload");
+        $this->setDescription("Upload a video");
+        $this->addArgument(
+            "filename",
+            InputArgument::REQUIRED,
+            "The file being uploaded"
+        );
+
+        parent::configure();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $cloud = $this->getCloud($input);
+        $output->writeln("Starting file upload...");
+        $cloud->encodeVideoFile($input->getArgument("filename"));
+        $output->writeln("File successfully uploaded.");
+    }
+}
