c8ddd408ea3719cbbaab33d00003c616c963aad9
Christian Flothmann <christian.flothmann@xabbuh.de>
2013-01-15 14:07:03 +0100

    fix root node name

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -24,7 +24,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root("xabbuh_pandastream_encoder");
+        $rootNode = $treeBuilder->root("xabbuh_panda");
         
         $rootNode
             ->children()
