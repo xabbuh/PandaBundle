19b1772adae2b9ce7e7cce7096755bb14682310a
Christian Flothmann <christian.flothmann@xabbuh.de>
2013-02-19 22:31:01 +0100

    video uploader v2 implementation using jQuery

---
+++ Resources/public/video-uploader/jquery-v2.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    $("input[panda-uploader=v2]").each(function () {
+        var authoriseUrl = $(this).attr("authorise-url");
+        var browseButtonId = $(this).attr("browse-button-id");
+        var cancelButtonId = $(this).attr("cancel-button-id");
+        var progressBarId = $(this).attr("progress-bar-id");
+        var progressBar = $("#" + progressBarId);
+
+        var currentFile = null;
+
+        var uploader = panda.uploader.init({
+            "buttonId": browseButtonId,
+            "authorizeUrl": authoriseUrl,
+            "progressBarId": progressBarId,
+            "onStart": function(file) {
+                progressBar.show();
+                currentFile = file;
+            },
+            "onProgress": function(file, percent) {
+                progressBar.text(percent + "%");
+            },
+            "onCancel": function() {
+                progressBar.hide();
+            },
+            "onComplete": function() {
+                progressBar.hide();
+            }
+        });
+
+        $("#" + cancelButtonId).click(function() {
+            uploader.cancel(currentFile);
+        });
+    });
+});
