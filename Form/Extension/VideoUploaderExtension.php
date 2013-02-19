87aaaf9882fa8a64d4069acfc7968f32e603f1d7
Christian Flothmann <christian.flothmann@xabbuh.de>
2013-02-19 22:29:47 +0100

    add panda upload widget FileType modifications

--- Form/Extension/VideoUploaderExtension.php
+++ Form/Extension/VideoUploaderExtension.php
@@ -15,6 +15,7 @@ use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Routing\Router;
 
 /**
  * Extends the {@link http://api.symfony.com/master/Symfony/Component/Form/Extension/Core/Type/FileType.html FileType}
@@ -25,6 +26,23 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
  */
 class VideoUploaderExtension extends AbstractTypeExtension
 {
+    /**
+     * The router service
+     * @var \Symfony\Component\Routing\Router
+     */
+    private $router;
+
+
+    /**
+     * Constructor.
+     *
+     * @param \Symfony\Component\Routing\Router $router The router
+     */
+    public function __construct(Router $router)
+    {
+        $this->router = $router;
+    }
+
     /**
      * {@inheritDoc}
      */
@@ -38,7 +56,7 @@ class VideoUploaderExtension extends AbstractTypeExtension
      */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
-        $resolver->setOptional(array("panda_widget", "panda_widget_version"));
+        $resolver->setOptional(array("panda_widget", "panda_widget_version", "cloud"));
         $resolver->setAllowedValues(
             array("panda_widget_version" => array(1, 2))
         );
@@ -50,13 +68,38 @@ class VideoUploaderExtension extends AbstractTypeExtension
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
         if (isset($options["panda_widget"]) && $options["panda_widget"]) {
+            $view->vars["panda_uploader"] = true;
+
             if (isset($options["panda_widget_version"])) {
                 $widgetVersion = $options["panda_widget_version"];
             } else {
                 $widgetVersion = 2;
             }
 
-            $view->vars["attr"]["panda_uploader"] = "v$widgetVersion";
+            // generate widgets' ids
+            $formId = $view->vars["id"];
+            $browseButtonId = "browse_button_$formId";
+            $cancelButtonId = "cancel_button_$formId";
+            $progressBarId = "progress_bar_$formId";
+
+            // set input field attributes accordingly (widget ids will be
+            // read from there by the javascript uploader implementation)
+            $view->vars["attr"]["panda-uploader"] = "v$widgetVersion";
+            $view->vars["attr"]["authorise-url"] = $this->router->generate(
+                "xabbuh_panda_authorise_upload",
+                array("cloud" => $options["cloud"])
+            );
+            $view->vars["attr"]["browse-button-id"] = $browseButtonId;
+            $view->vars["attr"]["cancel-button-id"] = $cancelButtonId;
+            $view->vars["attr"]["progress-bar-id"] = $progressBarId;
+
+            $view->vars["browse_button_id"] = $browseButtonId;
+            $view->vars["browse_button_label"] = "Browse";
+            $view->vars["cancel_button_id"] = $cancelButtonId;
+            $view->vars["cancel_button_label"] = "Cancel";
+            $view->vars["progress_bar_id"] = $progressBarId;
+        } else {
+            $view->vars["panda_uploader"] = false;
         }
     }
 }
