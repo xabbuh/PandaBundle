7c1cd84439774e75178ee7865e7fd19296f52512
Christian Flothmann <christian.flothmann@xabbuh.de>
2014-07-15 22:39:52 +0200

    CS: header docblock indentation

--- Tests/Form/Extension/VideoUploaderExtensionTest.php
+++ Tests/Form/Extension/VideoUploaderExtensionTest.php
@@ -1,13 +1,13 @@
 <?php
 
 /*
-* This file is part of the XabbuhPandaBundle package.
-*
-* (c) Christian Flothmann <christian.flothmann@xabbuh.de>
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
-*/
+ * This file is part of the XabbuhPandaBundle package.
+ *
+ * (c) Christian Flothmann <christian.flothmann@xabbuh.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Xabbuh\PandaBundle\Tests\Form\Extension;
 
